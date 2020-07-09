a3330b359298cf7c6b87d7fb9ec3c65aea75a15e
Greg Burd <greg@blockfi.com>
2020-07-09 10:21:28 -0400

    Fixes, updates and changes from Splunk/HEC to Humio/HEC.

--- humio.go
+++ humio.go
@@ -1,4 +1,4 @@
-package splunk
+package humio
 
 import (
 	"bytes"
@@ -22,7 +22,7 @@ import (
 )
 
 func init() {
-	router.AdapterFactories.Register(NewSplunkAdapter, "splunk")
+	router.AdapterFactories.Register(NewHumioAdapter, "humio")
 }
 
 func debug(v ...interface{}) {
@@ -51,7 +51,7 @@ func getIntParameter(
 	if value, ok := options[parameterName]; ok {
 		valueInt, err := strconv.Atoi(value)
 		if err != nil {
-			debug("splunk: invalid value for parameter:", parameterName, value)
+			debug("humio: invalid value for parameter:", parameterName, value)
 			return dfault
 		} else {
 			return valueInt
@@ -68,7 +68,7 @@ func getDurationParameter(
 	if value, ok := options[parameterName]; ok {
 		valueDuration, err := time.ParseDuration(value)
 		if err != nil {
-			debug("splunk: invalid value for parameter:", parameterName, value)
+			debug("humio: invalid value for parameter:", parameterName, value)
 			return dfault
 		} else {
 			return valueDuration
@@ -81,15 +81,15 @@ func getDurationParameter(
 func dial(netw, addr string) (net.Conn, error) {
 	dial, err := net.Dial(netw, addr)
 	if err != nil {
-		debug("splunk: new dial", dial, err, netw, addr)
+		debug("humio: new dial", dial, err, netw, addr)
 	} else {
-		debug("splunk: new dial", dial, netw, addr)
+		debug("humio: new dial", dial, netw, addr)
 	}
 	return dial, err
 }
 
-// SplunkAdapter is an adapter that POSTs logs to an HTTP endpoint
-type SplunkAdapter struct {
+// HumioAdapter is an adapter that POSTs logs to an HTTP endpoint
+type HumioAdapter struct {
 	route             *router.Route
 	url               string
 	client            *http.Client
@@ -101,22 +101,22 @@ type SplunkAdapter struct {
 	bufferMutex       sync.Mutex
 	useGzip           bool
 	crash		  bool
-	splunkToken	  string
-	splunkIndex	  string
-	splunkSourcetype  string
+	humioToken	  string
+	humioIndex	  string
+	humioSourcetype   string
 }
 
-// NewSplunkAdapter creates an SplunkAdapter
-func NewSplunkAdapter(route *router.Route) (router.LogAdapter, error) {
+// NewHumioAdapter creates an HumioAdapter
+func NewHumioAdapter(route *router.Route) (router.LogAdapter, error) {
 
 	// Figure out the URI and create the HTTP client
-	path := "/services/collector"
-	if os.Getenv("SPLUNK_HEC_PATH") != "" {
-		path = os.Getenv("SPLUNK_HEC_PATH")
+	path := "/api/v1/ingest/hec"
+	if os.Getenv("HUMIO_HEC_PATH") != "" {
+		path = os.Getenv("HUMIO_HEC_PATH")
 	}
 
 	endpointUrl := fmt.Sprintf("https://%s%s", route.Address, path)
-	debug("splunk: url:", endpointUrl)
+	debug("humio: url:", endpointUrl)
 	transport := &http.Transport{}
 	transport.Dial = dial
 
@@ -126,28 +126,31 @@ func NewSplunkAdapter(route *router.Route) (router.LogAdapter, error) {
 	if proxyUrlString != "" {
 		proxyUrl, err := url.Parse(proxyUrlString)
 		if err != nil {
-			die("", "splunk: cannot parse proxy url:", err, proxyUrlString)
+			die("", "humio: cannot parse proxy url:", err, proxyUrlString)
 		}
 		transport.Proxy = http.ProxyURL(proxyUrl)
 		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-		debug("splunk: proxy url:", proxyUrl)
+		debug("humio: proxy url:", proxyUrl)
 	}
 
-	// Figure out the Splunk specifig settings
-	splunkToken := os.Getenv("SPLUNK_TOKEN")
+	// Figure out the Humio specifig settings
+	humioToken := os.Getenv("HUMIO_INGEST_TOKEN")
+	if humioToken == "" {
+		die("", "humio: HUMIO_INGEST_TOKEN is required but was not set")
+	}
 
-	if os.Getenv("SPLUNK_INSECURE") == "true" {
+	if os.Getenv("HUMIO_TLS") == "false" {
 		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 
-	splunkIndex := "main"
-        if os.Getenv("SPLUNK_INDEX") != "" {
-		splunkIndex = os.Getenv("SPLUNK_INDEX")
+	humioIndex := "main"
+        if os.Getenv("HUMIO_INDEX") != "" {
+		humioIndex = os.Getenv("HUMIO_INDEX")
 	}
 
-        splunkSourcetype := "docker"
-        if os.Getenv("SPLUNK_SOURCETYPE") != "" {
-                splunkSourcetype = os.Getenv("SPLUNK_SOURCETYPE")
+        humioSourcetype := "docker"
+        if os.Getenv("HUMIO_SOURCETYPE") != "" {
+                humioSourcetype = os.Getenv("HUMIO_SOURCETYPE")
         }
 
 	// Create the client
@@ -155,10 +158,9 @@ func NewSplunkAdapter(route *router.Route) (router.LogAdapter, error) {
 
 	// Determine the buffer capacity
 	defaultCapacity := 100
-	capacity := getIntParameter(
-		route.Options, "http.buffer.capacity", defaultCapacity)
+	capacity := getIntParameter(route.Options, "http.buffer.capacity", defaultCapacity)
 	if capacity < 1 || capacity > 10000 {
-		debug("splunk: non-sensical value for parameter: http.buffer.capacity",
+		debug("humio: non-sensical value for parameter: http.buffer.capacity",
 			capacity, "using default:", defaultCapacity)
 		capacity = defaultCapacity
 	}
@@ -166,34 +168,34 @@ func NewSplunkAdapter(route *router.Route) (router.LogAdapter, error) {
 
 	// Determine the buffer timeout
 	defaultTimeout, _ := time.ParseDuration("1000ms")
-	timeout := getDurationParameter(
-		route.Options, "http.buffer.timeout", defaultTimeout)
+	timeout := getDurationParameter(route.Options, "http.buffer.timeout", defaultTimeout)
 	timeoutSeconds := timeout.Seconds()
 	if timeoutSeconds < .1 || timeoutSeconds > 600 {
-		debug("splunk: non-sensical value for parameter: http.buffer.timeout",
+		debug("humio: non-sensical value for parameter: http.buffer.timeout",
 			timeout, "using default:", defaultTimeout)
 		timeout = defaultTimeout
 	}
 	timer := time.NewTimer(timeout)
 
 	// Figure out whether we should use GZIP compression
-	useGzip := false
+	useGzip := true
 	useGZipString := getStringParameter(route.Options, "http.gzip", "false")
 	if useGZipString == "true" {
 		useGzip = true
-		debug("splunk: gzip compression enabled")
+		debug("humio: gzip compression enabled")
 	}
 
 	// Should we crash on an error or keep going?
-	crash := true
+	var crash = false
+	if os.Getenv("DEBUG") != "" { crash = true } else { crash = false }
 	crashString := getStringParameter(route.Options, "http.crash", "true")
 	if crashString == "false" {
 		crash = false
-		debug("splunk: don't crash, keep going")
+		debug("humio: don't crash, keep going")
 	}
 
 	// Make the HTTP adapter
-	return &SplunkAdapter{
+	return &HumioAdapter{
 		route:    route,
 		url:      endpointUrl,
 		client:   client,
@@ -203,14 +205,14 @@ func NewSplunkAdapter(route *router.Route) (router.LogAdapter, error) {
 		timeout:  timeout,
 		useGzip:  useGzip,
 		crash:    crash,
-		splunkToken:		splunkToken,
-		splunkIndex:		splunkIndex,
-		splunkSourcetype:	splunkSourcetype,
+		humioToken:		humioToken,
+		humioIndex:		humioIndex,
+		humioSourcetype:	humioSourcetype,
 	}, nil
 }
 
 // Stream implements the router.LogAdapter interface
-func (a *SplunkAdapter) Stream(logstream chan *router.Message) {
+func (a *HumioAdapter) Stream(logstream chan *router.Message) {
 	for {
 		select {
 		case message := <-logstream:
@@ -233,7 +235,7 @@ func (a *SplunkAdapter) Stream(logstream chan *router.Message) {
 }
 
 // Flushes the accumulated messages in the buffer
-func (a *SplunkAdapter) flushHttp(reason string) {
+func (a *HumioAdapter) flushHttp(reason string) {
 
 	// Stop the timer and drain any possible remaining events
 	a.timer.Stop()
@@ -260,22 +262,21 @@ func (a *SplunkAdapter) flushHttp(reason string) {
 	messages := make([]string, 0, len(buffer))
 	for i := range buffer {
 		m := buffer[i]
-		splunkMessageEvent := SplunkMessageEvent{Message: m.Data}
-		if os.Getenv("SPLUNK_DOCKER_LABELS") != "" {
-			splunkMessageEvent.Labels = make(map[string]string)
+		humioMessageEvent := HumioMessageEvent{Message: m.Data}
+		if os.Getenv("HUMIO_DOCKER_LABELS") != "" {
+			humioMessageEvent.Labels = make(map[string]string)
 			for label, value := range m.Container.Config.Labels {
-				splunkMessageEvent.Labels[strings.Replace(label, ".", "_", -1)] = value
+				humioMessageEvent.Labels[strings.Replace(label, ".", "_", -1)] = value
 			}
 		}
-		splunkMessage := SplunkMessage{
-			Time:		m.Time.Unix(),
+		humioMessage := HumioMessage{
+			Time:           m.Time.Unix(), //TODO: m.Time.Format(time.RFC3339Nano), https://github.com/rickalm/logspout-gelf/blob/master/gelf.go#L54
 			Hostname:	m.Container.Config.Hostname,
 			Source:		m.Source,
-			SourceType:	a.splunkSourcetype,
-			Index:		a.splunkIndex,
-			Event:		splunkMessageEvent,
+			SourceType:	a.humioSourcetype,
+			Event:		humioMessageEvent,
 		}
-		message, err := json.Marshal(splunkMessage)
+		message, err := json.Marshal(humioMessage)
 		if err != nil {
 			debug("flushHttp - Error encoding JSON: ", err)
 			continue
@@ -289,22 +290,22 @@ func (a *SplunkAdapter) flushHttp(reason string) {
 	go func() {
 
 		// Create the request and send it on its way
-		request := createRequest(a.url, a.useGzip, a.splunkToken, payload)
+		request := createRequest(a.url, a.useGzip, a.humioToken, payload)
 		start := time.Now()
 		response, err := a.client.Do(request)
 		if err != nil {
-			debug("splunk - error on client.Do:", err, a.url)
-			// TODO @raychaser - now what?
+			debug("humio - error on client.Do:", err, a.url)
+			// TODO now what?
 			if a.crash {
-				die("splunk - error on client.Do:", err, a.url)
+				die("humio - error on client.Do:", err, a.url)
 			} else {
-				debug("splunk: error on client.Do:", err)
+				debug("humio: error on client.Do:", err)
 			}
 		} else if response.StatusCode != 200 {
-			debug("splunk: response not 200 but", response.StatusCode)
-			// TODO @raychaser - now what?
+			debug("humio: response not 200 but", response.StatusCode)
+			// TODO now what?
 			if a.crash {
-				die("splunk: response not 200 but", response.StatusCode)
+				die("humio: response not 200 but", response.StatusCode)
 			}
 		}
 
@@ -318,62 +319,57 @@ func (a *SplunkAdapter) flushHttp(reason string) {
 		// Bookkeeping, logging
 		timeAll := time.Since(start)
 		a.totalMessageCount += len(messages)
-		debug("splunk: flushed:", reason, "messages:", len(messages),
+		debug("humio: flushed:", reason, "messages:", len(messages),
 			"in:", timeAll, "total:", a.totalMessageCount)
 	}()
 }
 
 // Create the request based on whether GZIP compression is to be used
-func createRequest(url string, useGzip bool, splunkToken string, payload string) *http.Request {
+func createRequest(url string, useGzip bool, humioToken string, payload string) *http.Request {
 	var request *http.Request
 	if useGzip {
 		gzipBuffer := new(bytes.Buffer)
 		gzipWriter := gzip.NewWriter(gzipBuffer)
 		_, err := gzipWriter.Write([]byte(payload))
 		if err != nil {
-			// TODO @raychaser - now what?
-			die("splunk: unable to write to GZIP writer:", err)
+			die("humio: unable to write to GZIP writer:", err)
 		}
 		err = gzipWriter.Close()
 		if err != nil {
-			// TODO @raychaser - now what?
-			die("splunk: unable to close GZIP writer:", err)
+			die("humio: unable to close GZIP writer:", err)
 		}
 		request, err = http.NewRequest("POST", url, gzipBuffer)
 		if err != nil {
-			debug("splunk: error on http.NewRequest:", err, url)
-			// TODO @raychaser - now what?
-			die("", "splunk: error on http.NewRequest:", err, url)
+			die("", "humio: error on http.NewRequest:", err, url)
 		}
 		request.Header.Set("Content-Encoding", "gzip")
 	} else {
 		var err error
 		request, err = http.NewRequest("POST", url, strings.NewReader(payload))
 		if err != nil {
-			debug("splunk: error on http.NewRequest:", err, url)
-			// TODO @raychaser - now what?
-			die("", "splunk: error on http.NewRequest:", err, url)
+			die("", "humio: error on http.NewRequest:", err, url)
 		}
 	}
 
-	if (splunkToken != "") {
-                 request.Header.Set("Authorization", "Splunk " + splunkToken)
+	if (humioToken != "") {
+		request.Header.Set("Authorization", "Bearer " + humioToken)
         }
+
+	request.Header.Set("Content-Type", "text/plain; charset=utf-8")
 
 	return request
 }
 
-type SplunkMessageEvent struct {
+type HumioMessageEvent struct {
         Message         string			`json:"message"`
 	Labels		map[string]string	`json:"labels"`
 }
 
-// SplunkMessage is a simple JSON representation of the log message.
-type SplunkMessage struct {
+// HumioMessage is a simple JSON representation of the log message.
+type HumioMessage struct {
 	Time		int64 `json:"time"`
 	Source		string `json:"source"`
 	SourceType	string `json:"sourcetype"`
-	Index		string `json:"index"`
 	Hostname	string `json:"host"`
-	Event		SplunkMessageEvent	`json:"event"`
+	Event		HumioMessageEvent	`json:"event"`
 }
